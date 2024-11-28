5e025ac817e2f655aeee287dd73e6bd63bbcfea2
Vatsal Gupta <tsrvatsal17@gmail.com>
2024-11-28 20:51:53 +0530

    All UI Ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/cart_page.dart';
 import 'package:shop_app/product_list.dart';
 
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -14,7 +15,7 @@ class _HomePageState extends State<HomePage> {
 
   List<Widget> pages = const [
     ProductList(),
-    CartPage()
+    CartPage(),
   ];
 
   @override
@@ -31,8 +32,10 @@ class _HomePageState extends State<HomePage> {
             BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
             BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
           ]),
+          
       body: IndexedStack(
         index: currentPage,
+        children: pages,
       ),
     );
   }
